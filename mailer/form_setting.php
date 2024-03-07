f6661a47f467bfd221c042af61c6fb6bc4dfbec9
ahmedMostafaGenesis <85011552+ahmedmostaafa@users.noreply.github.com>
2024-03-07 04:30:02 +0200

    mail

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -1,7 +1,7 @@
 <?php
 	/*Form settings*/
-	$subj = "New message from mr resume site"; //letter subject
-	$to = 'laggindiegamestudio@gmail.com'; // Enter Your E-mail
+	$subj = "New message from my Portfolio site"; //letter subject
+	$to = 'thdragon7ell@gmail.com'; // Enter Your E-mail
 	$from = 'laggindiegamestudio@gmail.com'; // Admin e-mail
 	$fromName = 'Ahmed Mostafa'; // Your company name
 	$charset = 'UTF-8';
